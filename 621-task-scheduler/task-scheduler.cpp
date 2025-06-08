bd07fc3c1ffa5ba9520619d790b1e7b6efd6a3a7
skjaiswal88 <119606060+skjaiswal88@users.noreply.github.com>
2025-06-08 16:46:31 +0530

    Time: 47 ms (15.49%) | Memory: 47.7 MB (18.23%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -2,33 +2,41 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
         vector<int> freq(26, 0);
-        
-        // Count frequency of each task
-        for (char task : tasks) {
+        for(char task : tasks) {
             freq[task - 'A']++;
         }
-        
-        // Find max frequency
-        int max_freq = *max_element(freq.begin(), freq.end());
-        
-        // Count how many tasks have max frequency
-        int max_count = 0;
-        for (int f : freq) {
-            if (f == max_freq) {
-                max_count++;
+
+        priority_queue<int> pq;
+        for(int f : freq) {
+            if(f > 0) pq.push(f);
+        }
+
+        int intervals = 0;
+        int cycle = n + 1;
+
+        while(!pq.empty()) {
+            vector<int> temp;
+            int count = 0;
+
+            for(int i = 0; i < cycle; i++) {
+                if(!pq.empty()) {
+                    int top = pq.top();
+                    pq.pop();
+                    if(top > 1) temp.push_back(top - 1);
+                    intervals++;
+                    count++;
+                } else {
+                    // If no tasks left in this cycle break early if no tasks remain
+                    if(temp.empty()) break;
+                    intervals++;  // idle interval
+                }
+            }
+
+            for(int t : temp) {
+                pq.push(t);
             }
         }
-        
-        // Calculate the minimum intervals required
-        int part_count = max_freq - 1;
-        int part_length = n - (max_count - 1);
-        int empty_slots = part_count * part_length;
-        int available_tasks = tasks.size() - max_freq * max_count;
-        int idles = max(0, empty_slots - available_tasks);
-        
-        return tasks.size() + idles;
-        
-        // Alternatively, you can use the formula below:
-        // return max((int)tasks.size(), (max_freq - 1) * (n + 1) + max_count);
+
+        return intervals;
     }
 };
