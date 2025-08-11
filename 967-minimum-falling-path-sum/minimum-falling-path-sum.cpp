8f3e1438e72b5a122a668202f4720dc14728fc9c
skjaiswal88 <119606060+skjaiswal88@users.noreply.github.com>
2025-08-11 19:28:02 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (77.62%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
 
-    int fun(vector<vector<int>>& matrix,int i,int j,int n,vector<vector<int>>& dp){
-        if(j < 0 || j >= n) return INT_MAX; 
-        if(i == n - 1) return matrix[i][j]; 
+    // int fun(vector<vector<int>>& matrix,int i,int j,int n,vector<vector<int>>& dp){
+    //     if(j < 0 || j >= n) return INT_MAX; 
+    //     if(i == n - 1) return matrix[i][j]; 
 
-        if(dp[i][j] != 100000) return dp[i][j];  
+    //     if(dp[i][j] != 100000) return dp[i][j];  
 
-        int a = fun(matrix,i+1,j-1,n,dp);
-        int b = fun(matrix,i+1,j,n,dp);
-        int c = fun(matrix,i+1,j+1,n,dp);
+    //     int a = fun(matrix,i+1,j-1,n,dp);
+    //     int b = fun(matrix,i+1,j,n,dp);
+    //     int c = fun(matrix,i+1,j+1,n,dp);
 
 
-        return dp[i][j]=matrix[i][j] + min(a, min(b, c));
+    //     return dp[i][j]=matrix[i][j] + min(a, min(b, c));
 
-    }
+    // }
 
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
@@ -23,9 +23,27 @@ public:
         vector<vector<int>> dp(n,vector<int>(n,100000));
 
         for(int i=0;i<n;i++){
-            an=min(an,fun(matrix,0,i,n,dp));
+            // an=min(an,fun(matrix,0,i,n,dp));
+            dp[0][i]=matrix[0][i];
         }
 
-        return an;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+
+                int a=INT_MAX; int b=dp[i-1][j]; int c=INT_MAX;
+
+                if(j-1>=0){
+                    a=dp[i-1][j-1];
+                }
+                if(j+1<n){
+                    c=dp[i-1][j+1];
+                }
+
+                dp[i][j]=matrix[i][j] + min(a, min(b, c));
+            }
+        }
+
+        return *min_element(dp[n-1].begin(),dp[n-1].end());
+
     }
 };
