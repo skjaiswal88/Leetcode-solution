8e3b2dde0a6719c2a0825d4609099d04436f3b48
skjaiswal88 <119606060+skjaiswal88@users.noreply.github.com>
2025-10-11 09:49:41 +0530

    Time: 279 ms (70.37%) | Memory: 221.2 MB (28.60%) - LeetSync

---
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long maximumTotalDamage(vector<int>& power) {
+        unordered_map<long long, long long> freq;
+        for (int p : power) freq[p] += p;
+        vector<long long> vals;
+        for (auto &x : freq) vals.push_back(x.first);
+        sort(vals.begin(), vals.end());
+        int n = vals.size();
+        vector<long long> dp(n, 0);
+        dp[0] = freq[vals[0]];
+        for (int i = 1; i < n; i++) {
+            long long take = freq[vals[i]];
+            int j = i - 1;
+            while (j >= 0 && vals[i] - vals[j] <= 2) j--;
+            if (j >= 0) take += dp[j];
+            dp[i] = max(dp[i - 1], take);
+        }
+        return dp.back();
+    }
+};
